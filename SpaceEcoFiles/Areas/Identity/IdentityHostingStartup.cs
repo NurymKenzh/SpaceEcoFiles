0ab91dac7879f41dd6642b98f2da6e010c1db760
Nurym Kenzh <nak290284@gmail.com>
2020-11-13 21:33:33 +0600

    Users

---
+++ SpaceEcoFiles/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SpaceEcoFiles.Data;
+
+[assembly: HostingStartup(typeof(SpaceEcoFiles.Areas.Identity.IdentityHostingStartup))]
+namespace SpaceEcoFiles.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
